8f452705a2bffbe41f49a6226bad7c2833055d0d
JohnBartleyDev <bartleyj5064@gmai.com>
2023-04-02 13:33:30 -0400

    Signed-off-by: JohnBartleyDev <bartleyj5064@gmai.com>

--- Amberdata/historicalData.js
+++ Amberdata/historicalData.js
@@ -15,26 +15,26 @@ while (!userrequest) {
     ETH_USDC();
     userrequest = true;
   } 
-  else if(request == 'KISHU_WETH') {
-    KISHU_WETH()
-    userrequest = true;
-  }
-  else if(request == 'WETH_TRU') {
-    WETH_TRU()
-    userrequest = true;
-  }
-  else if(request == 'WISE_ETH') {
-    WISE_ETH()
-    userrequest = true;
-  }
-  else if(request == 'ELON_ETH') {
-    ELON_WETH()
-    userrequest = true;
-  }
-  else if(request == 'DAI_USDC') {
-    DAI_USDC()
-    userrequest = true;
-  }
+  // else if(request == 'KISHU_WETH') {
+  //   KISHU_WETH()
+  //   userrequest = true;
+  // }
+  // else if(request == 'WETH_TRU') {
+  //   WETH_TRU()
+  //   userrequest = true;
+  // }
+  // else if(request == 'WISE_ETH') {
+  //   WISE_ETH()
+  //   userrequest = true;
+  // }
+  // else if(request == 'ELON_ETH') {
+  //   ELON_WETH()
+  //   userrequest = true;
+  // }
+  // else if(request == 'DAI_USDC') {
+  //   DAI_USDC()
+  //   userrequest = true;
+  // }
   else{
     console.log('Incorrect Pair, defaulting to ETH-USDC\n');
     ETH_USDC();
@@ -60,74 +60,74 @@ function ETH_USDC() {
   });
 }
 // THESE still rely on amber data api which does not currently work
-function  DAI_WETH() {
-  sdk.defiOhlcvHistorical({
-    exchange: 'uniswapv3',
-    startDate: '2023-01-01',
-    endDate: '2023-03-21',
-    timeInterval: 'days',
-    format: 'json',
-    pool: 'DAI_WETH'
-  })
-    .then(({ data }) => console.log(data))
-    .catch(err => console.error(err));
-}
+// function  DAI_WETH() {
+//   sdk.defiOhlcvHistorical({
+//     exchange: 'uniswapv3',
+//     startDate: '2023-01-01',
+//     endDate: '2023-03-21',
+//     timeInterval: 'days',
+//     format: 'json',
+//     pool: 'DAI_WETH'
+//   })
+//     .then(({ data }) => console.log(data))
+//     .catch(err => console.error(err));
+// }
 
-function  KISHU_WETH() {
-    sdk.defiOhlcvHistorical({
-        exchange: 'uniswapv3',
-        startDate: '2023-01-01',
-        endDate: '2023-03-21',
-        format: 'csv',
-        pool: 'KISHU_WETH'
-    })
-        .then(({ data }) => console.log(data))
-        .catch(err => console.error(err));
-}
+// function  KISHU_WETH() {
+//     sdk.defiOhlcvHistorical({
+//         exchange: 'uniswapv3',
+//         startDate: '2023-01-01',
+//         endDate: '2023-03-21',
+//         format: 'csv',
+//         pool: 'KISHU_WETH'
+//     })
+//         .then(({ data }) => console.log(data))
+//         .catch(err => console.error(err));
+// }
 
-function  WETH_TRU() {
-    sdk.defiOhlcvHistorical({
-        exchange: 'uniswapv3',
-        startDate: '2023-01-01',
-        endDate: '2023-03-21',
-        format: 'csv',
-        pool: 'WETH_TRU'
-    })
-        .then(({ data }) => console.log(data))
-        .catch(err => console.error(err));
-}
-function  WISE_ETH() {
-    sdk.defiOhlcvHistorical({
-        exchange: 'uniswapv3',
-        startDate: '2023-01-01',
-        endDate: '2023-03-21',
-        format: 'csv',
-        pool: 'WISE_ETH'
-    })
-        .then(({ data }) => console.log(data))
-        .catch(err => console.error(err));
-}
+// function  WETH_TRU() {
+//     sdk.defiOhlcvHistorical({
+//         exchange: 'uniswapv3',
+//         startDate: '2023-01-01',
+//         endDate: '2023-03-21',
+//         format: 'csv',
+//         pool: 'WETH_TRU'
+//     })
+//         .then(({ data }) => console.log(data))
+//         .catch(err => console.error(err));
+// }
+// function  WISE_ETH() {
+//     sdk.defiOhlcvHistorical({
+//         exchange: 'uniswapv3',
+//         startDate: '2023-01-01',
+//         endDate: '2023-03-21',
+//         format: 'csv',
+//         pool: 'WISE_ETH'
+//     })
+//         .then(({ data }) => console.log(data))
+//         .catch(err => console.error(err));
+// }
 
-function  ELON_WETH() {
-    sdk.defiOhlcvHistorical({
-        exchange: 'uniswapv3',
-        startDate: '2023-01-01',
-        endDate: '2023-03-21',
-        format: 'csv',
-        pool: 'ELON_WETH'
-    })
-        .then(({ data }) => console.log(data))
-        .catch(err => console.error(err));
-}
+// function  ELON_WETH() {
+//     sdk.defiOhlcvHistorical({
+//         exchange: 'uniswapv3',
+//         startDate: '2023-01-01',
+//         endDate: '2023-03-21',
+//         format: 'csv',
+//         pool: 'ELON_WETH'
+//     })
+//         .then(({ data }) => console.log(data))
+//         .catch(err => console.error(err));
+// }
 
-function  DAI_USDC() {
-    sdk.defiOhlcvHistorical({
-        exchange: 'uniswapv3',
-        startDate: '2023-01-01',
-        endDate: '2023-03-21',
-        format: 'csv',
-        pool: 'USD_ETH'
-    })
-        .then(({ data }) => console.log(data))
-        .catch(err => console.error(err));
-}
+// function  DAI_USDC() {
+//     sdk.defiOhlcvHistorical({
+//         exchange: 'uniswapv3',
+//         startDate: '2023-01-01',
+//         endDate: '2023-03-21',
+//         format: 'csv',
+//         pool: 'USD_ETH'
+//     })
+//         .then(({ data }) => console.log(data))
+//         .catch(err => console.error(err));
+// }
