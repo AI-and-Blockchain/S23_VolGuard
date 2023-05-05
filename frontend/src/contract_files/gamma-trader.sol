db4f2b8e843ba4d9fa08d559074e3f000eee92a1
WilliamHCarter <41378465+WilliamHCarter@users.noreply.github.com>
2023-05-04 23:26:43 -0400

    Update gamma-trader.sol

--- frontend/src/contract_files/gamma-trader.sol
+++ frontend/src/contract_files/gamma-trader.sol
@@ -1,17 +1,25 @@
 // SPDX-License-Identifier: Apache 2.0
 pragma solidity ^0.8.0;
 
-import "./simple-oracle.sol"; // Import the SimpleOracle contract
-import "./IUniswapV2Router02.sol"; // Import the Uniswap interface
-import "./IUniswapV2Router01.sol"; // Import the Uniswap interface
-
-import "./IERC20.sol"; // Import the ERC20 interface
+import "./simple-oracle.sol";
+import "./IUniswapV2Router02.sol";
+import "./IUniswapV2Router01.sol";
+import "./IERC20.sol";
 
 contract GammaTrader {
     SimpleOracle public oracle;
     IUniswapV2Router02 public uniswapRouter;
     address public ethAddress;
     address public usdcAddress;
+    address public owner;
+
+    event TradeExecuted(address indexed trader, uint256 amount, bool boughtETH);
+    event Withdrawal(address indexed to, address token, uint256 amount);
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Caller is not the owner");
+        _;
+    }
 
     constructor(
         address _oracleAddress,
@@ -23,25 +31,24 @@ contract GammaTrader {
         uniswapRouter = IUniswapV2Router02(_uniswapRouterAddress);
         ethAddress = _ethAddress;
         usdcAddress = _usdcAddress;
+        owner = msg.sender;
     }
 
-    function trade() public payable {
+    function trade(uint256 minAmountOut) public payable {
         uint256 storedNumber = oracle.getStoredNumber();
+        uint256 deadline = block.timestamp + 15;
 
         if (storedNumber > 1) {
-            // Buy ETH
-            uint256 deadline = block.timestamp + 15; // 15 seconds from now
-            uniswapRouter.swapExactTokensForETH(
+            uint[] memory amounts = uniswapRouter.swapExactTokensForETH(
                 msg.value,
-                0,
+                minAmountOut,
                 getPathForTokenToETH(),
                 address(this),
                 deadline
             );
+            emit TradeExecuted(msg.sender, amounts[0], true);
         } else {
-            // Swap for ETH-USDC LP
-            uint256 deadline = block.timestamp + 15; // 15 seconds from now
-            uniswapRouter.addLiquidityETH{value: msg.value}(
+            (uint amountToken, uint amountETH, ) = uniswapRouter.addLiquidityETH{value: msg.value}(
                 usdcAddress,
                 msg.value,
                 0,
@@ -49,6 +56,7 @@ contract GammaTrader {
                 address(this),
                 deadline
             );
+            emit TradeExecuted(msg.sender, amountETH, false);
         }
     }
 
@@ -60,14 +68,16 @@ contract GammaTrader {
     }
 
     // Function to withdraw accidentally sent tokens
-    function withdrawTokens(address _tokenAddress) external {
+    function withdrawTokens(address _tokenAddress) external onlyOwner {
         uint256 balance = IERC20(_tokenAddress).balanceOf(address(this));
         IERC20(_tokenAddress).transfer(msg.sender, balance);
+        emit Withdrawal(msg.sender, _tokenAddress, balance);
     }
 
     // Function to withdraw accidentally sent ETH
-    function withdrawETH() external {
+    function withdrawETH() external onlyOwner {
         uint256 balance = address(this).balance;
         payable(msg.sender).transfer(balance);
+        emit Withdrawal(msg.sender, ethAddress, balance);
     }
 }
